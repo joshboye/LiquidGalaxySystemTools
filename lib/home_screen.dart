c0ae13fbcccf711449e5607f4c39ea0b252999da
joshboye <joshboye8@gmail.com>
2023-06-24 02:11:10 +0530

    settings screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:liquidgalaxybasic/cleaning_screen.dart';
+import 'package:liquidgalaxybasic/settings_screen.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -20,61 +21,76 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFF07040A),
-        appBar: AppBar(
-          centerTitle: true,
+      home: Builder(
+        builder: (context) => Scaffold(
           backgroundColor: Color(0xFF07040A),
-          title: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Padding(padding: EdgeInsets.only(top: 7)),
-              SvgPicture.asset(
-                'images/lglogo.svg',
-                height: 35,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(bottom: 5),
-                child: Text(
-                  'System Tools',
-                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsScreen(),
+                    ),
+                  );
+                },
+                icon: SvgPicture.asset('images/icons/settingicon.svg'),
+              )
+            ],
+            centerTitle: true,
+            backgroundColor: Color(0xFF07040A),
+            title: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(padding: EdgeInsets.only(top: 7)),
+                SvgPicture.asset(
+                  'images/lglogo.svg',
+                  height: 35,
                 ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 5),
+                  child: Text(
+                    'System Tools',
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: tabs[_navitemindex],
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            backgroundColor: Color(0xFF07040A),
+            currentIndex: _navitemindex,
+            onTap: (index) {
+              setState(() {
+                _navitemindex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                // ignore: deprecated_member_use
+                icon: SvgPicture.asset(
+                  'images/icons/HomeIcon.svg',
+                  color: _navitemindex == 1 ? Color(0xFFAB0552) : Color(0xFFFCBA25),
+                ),
+                label: 'Home',
               ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'images/icons/Cleanlogo.svg',
+                  color: _navitemindex == 0 ? Color(0xFFAB0552) : Color(0xFFFCBA25),
+                ),
+                label: 'Tools',
+              )
             ],
           ),
-        ),
-        body: tabs[_navitemindex],
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          backgroundColor: Color(0xFF07040A),
-          currentIndex: _navitemindex,
-          onTap: (index) {
-            setState(() {
-              _navitemindex = index;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-              // ignore: deprecated_member_use
-              icon: SvgPicture.asset(
-                'images/icons/HomeIcon.svg',
-                color: _navitemindex == 1 ? Color(0xFFAB0552) : Color(0xFFFCBA25),
-              ),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                'images/icons/Cleanlogo.svg',
-                color: _navitemindex == 0 ? Color(0xFFAB0552) : Color(0xFFFCBA25),
-              ),
-              label: 'Tools',
-            )
-          ],
         ),
       ),
     );
@@ -115,7 +131,14 @@ class HomeBody extends StatelessWidget {
           width: 187,
           height: 50,
           child: TextButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsScreen(),
+                ),
+              );
+            },
             style: TextButton.styleFrom(foregroundColor: Color(0xFFAB0552)),
             child: const Text(
               'Can\'t connect? Check LG settings.',
---
+++ lib/settings_screen.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Color(0xFF07040A),
+          appBar: AppBar(
+            backgroundColor: Color(0xFF07040A),
+          ),
+          body: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset('images/icons/spanner.svg'),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const SizedBox(
+                    width: 300,
+                    child: Text(
+                      'Save Liquid Galaxy Connection Settings',
+                      style: TextStyle(
+                        color: Color(0xFF04E1FF),
+                        fontSize: 25,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                width: 350,
+                child: TextField(
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(27),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFD9D9D9),
+                          )),
+                      labelText: 'Username',
+                      labelStyle: const TextStyle(
+                        color: Color(0xFFD9D9D9),
+                      ),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(9.0),
+                        child: SvgPicture.asset('images/icons/usericon.svg'),
+                      ),
+                      prefixIconConstraints: const BoxConstraints(minHeight: 30, minWidth: 30)),
+                  style: const TextStyle(color: Color(0xFFD9D9D9)),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 350,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(27),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFD9D9D9),
+                          )),
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(
+                        color: Color(0xFFD9D9D9),
+                      ),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(9.0),
+                        child: SvgPicture.asset('images/icons/lockicon.svg'),
+                      ),
+                      prefixIconConstraints: const BoxConstraints(minHeight: 30, minWidth: 30)),
+                  style: const TextStyle(color: Color(0xFFD9D9D9)),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 350,
+                child: TextField(
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(27),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFD9D9D9),
+                          )),
+                      labelText: 'IP address',
+                      labelStyle: const TextStyle(
+                        color: Color(0xFFD9D9D9),
+                      ),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(9.0),
+                        child: SvgPicture.asset('images/icons/ipicon.svg'),
+                      ),
+                      prefixIconConstraints: const BoxConstraints(minHeight: 30, minWidth: 30)),
+                  style: const TextStyle(color: Color(0xFFD9D9D9)),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 350,
+                child: TextField(
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(27),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFD9D9D9),
+                          )),
+                      labelText: 'Port',
+                      labelStyle: const TextStyle(
+                        color: Color(0xFFD9D9D9),
+                      ),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(9.0),
+                        child: SvgPicture.asset('images/icons/porticon.svg'),
+                      ),
+                      prefixIconConstraints: const BoxConstraints(minHeight: 30, minWidth: 30)),
+                  style: const TextStyle(color: Color(0xFFD9D9D9)),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 150,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      shadowColor: Color(0xFF05ABA1),
+                      elevation: 30,
+                      backgroundColor: Color(0xFF05ABA1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset('images/icons/saveicon.svg'),
+                      const SizedBox(width: 10),
+                      const Text(
+                        'SAVE',
+                        style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
