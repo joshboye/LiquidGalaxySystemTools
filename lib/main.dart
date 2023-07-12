018e86e76f66c3fe995157534632cf089df9e398
joshboye <joshboye8@gmail.com>
2023-07-13 02:09:15 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:liquidgalaxybasic/screens/home_screen.dart';
+import 'package:liquidgalaxybasic/screens/splash_screen.dart';
 import 'package:liquidgalaxybasic/services/lg_service.dart';
 import 'package:liquidgalaxybasic/services/local_storage_service.dart';
 import 'package:liquidgalaxybasic/services/ssh_creds_storage_service.dart';
@@ -23,6 +23,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Homepage();
+    return const MaterialApp(
+      home: App(),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashScreen();
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:liquidgalaxybasic/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(const Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacement(_createRoute());
+    });
+  }
+
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => const Homepage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = const Offset(0.0, 1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF07040A),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'images/lglogo.svg',
+              height: 100,
+              width: 200,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              child: Text(
+                'Liquid Galaxy System Tools',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+              width: 150,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
